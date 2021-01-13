9d8e3a06024f0857af7c7f5779376bc145fc9523
Nicolas Crinon <n-crn@laposte.net>
2021-01-13 12:41:46 +0100

    is_leaf

--- src/Tree/Node.java
+++ src/Tree/Node.java
@@ -2,10 +2,53 @@ package Tree;
 
 import java.util.ArrayList;
 
+import data_structure.Stick;
+
 //Noeud de l'arbre
 public class Node extends Tree {
 	
-	// Fils du noeud
-	ArrayList<Node> sons = new ArrayList<Node>();
-
+	public Node (	ArrayList<Stick[]> lines) {
+		this.lines = lines;
+	}
+	
+	public boolean equals (Node node) {
+		ArrayList<Integer> groups1;
+		ArrayList<Integer> groups2;
+		for (int i = 0; i < lines.size(); i++) {
+			groups1 = new ArrayList<Integer>();
+			groups2 = new ArrayList<Integer>();
+			int compteur1 = 0;
+			int compteur2 = 0;
+			for (int j = 0; j < lines.get(i).length; j++) {
+				if(!lines.get(i)[j].getState()) {
+					compteur1++;
+				}
+				else if (compteur1 > 0){
+					groups1.add(compteur1);
+					compteur1 = 0;
+				}
+				if(!node.getLines().get(i)[j].getState()) {
+					compteur2++;
+				}
+				else if (compteur2 > 0){
+					groups2.add(compteur2);
+					compteur2 = 0;
+				}
+				if (groups1.size() == groups2.size()){
+					for (int k = 0; k < groups1.size(); k++) {
+						groups2.remove(groups1.get(k));
+					}
+					if (!groups2.isEmpty()) {
+						return false;
+					}
+				}
+				else {
+					return false;
+				}
+			}
+		}
+		return true;
+	
+	
+}
 }
--- src/Tree/Tree.java
+++ src/Tree/Tree.java
@@ -1,13 +1,54 @@
 package Tree;
 
-public class Tree {
+import java.util.ArrayList;
+
+import data_structure.Stick;
+
+public  class Tree {
+	
+	// Fils du noeud
+			ArrayList<Node> sons = new ArrayList<Node>();
+			// Lignes du jeu
+			ArrayList<Stick[]> lines = new ArrayList<Stick[]>();
 
 	// Chaque noeud et racine génèrent leurs propres fils
 	public void generateSons() {
 		// on génère tout l'arbre
 		
+		
+		
 		// on retient ce qu'on a déjà généré pour ne pas faire de doublon
 		
 	}
+
+	public ArrayList<Node> getSons() {
+		return sons;
+	}
+
+	public ArrayList<Stick[]> getLines() {
+		return lines;
+	}
+	
+	public boolean is_leaf() {
+
+		ArrayList<Integer> groups1;
+		ArrayList<Integer> groups2;
+		for (int i = 0; i < lines.size(); i++) {
+			for (int j = 0; j < lines.get(i).length; j++) {
+				if(lines.get(i)[j].getState()) {
+					return false;
+				}
+
+			}
+		}
+		
+		return true;
+	}
+
+	
+
+	
+	
+	
 	
 }
